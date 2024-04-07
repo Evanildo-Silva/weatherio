07aa06f9739500e7570f116d7895858c7a1bc06d
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-04-06 23:31:51 -0300

    feat: add mobile search toggle functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,30 @@
  * @copyright Evanildo-Silva 2024 All rights reserved
  * @author Evanildo-Silva <evanildo.sf91@gmail.com>
  */
+
+'use strict'
+
+/**
+ * Add event listener on multiole elements
+ * @param {NodeList} elements Elements node array
+ * @param {string} eventType Event type e.g.: "click", "mouseover"
+ * @param {Function} callback Callback function
+ */
+const addEventOnElements = function (elements, eventType, callback) {
+    for (const element of elements) {
+        element.addEventListener(eventType, callback)
+    }
+}
+
+/**
+ * Toggle search in mobile devices
+ */
+const searchView = document.querySelector("[data-search-view]")
+const searchTogglers = document.querySelectorAll("[data-search-toggle]")
+
+const toggleSearch = function () {
+    return searchView.classList.toggle("active")
+}
+
+
+addEventOnElements(searchTogglers, "click", toggleSearch)
