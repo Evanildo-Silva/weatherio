55f9b0d459904adbec41ffc96f841efb898fe94a
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-04-07 21:17:29 -0300

    feat: add current weather API integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@
 'use strict'
 
 import { fetchData, url } from "./api.js"
+import * as module from "./module.js"
 
 /**
  * Add event listener on multiole elements
@@ -27,7 +28,7 @@ const searchView = document.querySelector("[data-search-view]")
 const searchTogglers = document.querySelectorAll("[data-search-toggle]")
 
 const toggleSearch = function () {
-    return searchView.classList.toggle("active")
+    searchView.classList.toggle("active")
 }
 
 
@@ -82,8 +83,113 @@ searchField.addEventListener("input", function () {
                     searchResult.querySelector("[data-search-list]").appendChild(searchItem)
                     items.push(searchItem.querySelector("[data-search-toggle]"))
                 }
+
+                addEventOnElements(items, "click", function () {
+                    toggleSearch()
+                    searchResult.classList.remove("active")
+                })
+
             })
         }, searchTimeoutDuration)
     }
 
 })
+
+const container = document.querySelector("[data-container]")
+const loading = document.querySelector("[data-loading]")
+const currentLocationBtn = document.querySelector("[data-current-location-btn]")
+const errorContent = document.querySelector("[data-error-content]")
+
+/**
+ * Render all weather data in html
+ * @param {number} lat Latitude
+ * @param {number} lon Longitude
+ */
+export const updateWeather = function (lat, lon) {
+    // loading.style.display = "grid"
+    container.style.overflowY = "hidden"
+    container.classList.remove("fade-in")
+    errorContent.style.display = "none"
+
+    const currentWeatherSection = document.querySelector("[data-current-weather]")
+    const highlightSection = document.querySelector("[data-highlights]")
+    const hourlySection = document.querySelector("[data-hourly-forecast]")
+    const forecastSection = document.querySelector("[data-5-day-forecast]")
+
+    currentWeatherSection.innerHTML = ""
+    highlightSection.innerHTML = ""
+    hourlySection.innerHTML = ""
+    forecastSection.innerHTML = ""
+
+    if (window.location.hash === "#/current-location") {
+        currentLocationBtn.setAttribute("disabled", "")
+    } else {
+        currentLocationBtn.removeAttribute("disabled")
+    }
+
+    fetchData(url.currentWeather(lat, lon), function (currentWeather) {
+
+        const {
+            weather,
+            dt: dateUnix,
+            sys: {
+                sunrise: sunriseUnixUTC,
+                sunset: sunsetUnixUTC
+            },
+            main: {
+                temp,
+                feels_like,
+                pressure,
+                humidity
+            },
+            visibility,
+            timezone
+        } = currentWeather
+
+        const [{
+            description, icon
+        }] = weather
+
+        const card = document.createElement("div")
+        card.classList.add("card", "card-lg", "current-weather-card")
+
+        card.innerHTML = `
+            <h2 class="title-2 card-title">Clima atual</h2>
+
+            <div class="wrapper-heading">
+                <p class="heading">${parseInt(temp)}&deg;<sup>c</sup></p>
+
+                <img class="weather-icon" width="64" height="64" src="./assets/images/weather_icons/${icon}.png"
+                    alt="${description}">
+            </div>
+
+            <p class="body-3">${description}</p>
+
+            <ul class="meta-list">
+                <li class="meta-item">
+                    <span class="m-icon">calendar_today</span>
+
+                    <p class="title-3 meta-text">${module.getDate(dateUnix, timezone)}</p>
+                </li>
+
+                <li class="meta-item">
+                    <span class="m-icon">location_on</span>
+
+                    <p class="title-3 meta-text" data-location></p>
+                </li>
+            </ul>
+        `
+        fetchData(url.reverseGeo(lat, lon), function ([{ name, country }]) {
+            card.querySelector("[data-location]").innerHTML = `${name}, ${country}`
+        })
+
+        currentWeatherSection.appendChild(card)
+
+
+    })
+
+}
+
+export const error404 = function () {
+
+}
