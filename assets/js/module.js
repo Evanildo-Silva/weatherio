95c37abf3967572ae1fc6eb0549e34924a4552e9
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-04-07 23:17:15 -0300

    fix: add adjust in weekDayNames

--- assets/js/module.js
+++ assets/js/module.js
@@ -9,13 +9,12 @@
 
 export const weekDayNames = [
     "Domingo",
-    "Segunda feira",
-    "Terça feira",
-    "Quarta feira",
-    "Quinta feira",
-    "Sexta feira",
-    "Sábado",
-    "Domingo"
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
 ]
 
 export const monthNames = [
