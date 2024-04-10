9dfdec75ff8c415d88990c73bddd834d92074b16
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-04-10 20:58:24 -0300

    feat: add serverless functions

---
+++ serverless/api.js
@@ -0,0 +1,30 @@
+import fetch from "node-fetch";
+
+const { OPEN_WEATHER_API_KEY } = process.env
+
+exports.handler = async (event, context) => {
+    const { URL, callback } = event.queryStringParameters
+
+    try {
+        if (!URL || !callback) {
+            throw new Error("URL and callback are required parameters.");
+        }
+        const fetchData = await fetch(`${URL}&appid=${OPEN_WEATHER_API_KEY}&lang=pt_br`)
+        const fetchDataJson = await fetchData.json()
+
+        if (fetchDataJson.statusCode !== 200) {
+            throw new Error(`API returned an error: ${data.message}`);
+        }
+
+        callback(fetchDataJson)
+        return {
+            statusCode: 200,
+            body: JSON.stringify(fetchDataJson)
+        }
+    } catch (error) {
+        return {
+            statusCode: 422,
+            body: error.stack
+        }
+    }
+}
