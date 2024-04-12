7be02d138551ed55811a7dcde1a13745df6b4aa6
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-04-11 22:34:36 -0300

    fix: imports

--- serverless/current_weather.js
+++ serverless/current_weather.js
@@ -1,28 +1,28 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch"
 
-const { OPEN_WEATHER_API_KEY } = process.env;
+const { OPEN_WEATHER_API_KEY } = process.env
 
 exports.handler = async function (event, context) {
     try {
-        const { lat, lon } = event.body;
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${OPEN_WEATHER_API_KEY}&lang=pt_br`;
+        const { lat, lon } = JSON.parse(event.body)
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${OPEN_WEATHER_API_KEY}&lang=pt_br`
 
-        const response = JSON.parse(event.body);;
+        const response = await fetch(url)
 
         if (!response.ok) {
-            throw new Error(`Erro na requisição: ${response.status} ${response.statusText}`);
+            throw new Error(`Erro na requisição: ${response.status} ${response.statusText}`)
         }
 
-        const data = await response.json();
+        const data = await response.json()
 
         return {
             statusCode: 200,
             body: JSON.stringify(data)
-        };
+        }
     } catch (error) {
         return {
             statusCode: 500,
             body: JSON.stringify({ error: error.message })
-        };
+        }
     }
-};
+}
